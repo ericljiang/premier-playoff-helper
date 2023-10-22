5aa6d1ac10d35226e267aa670312820338ae744c
Eric Jiang <ejiang@amazon.com>
2023-10-21 21:28:05 -0700

    Name conferences and divisions.

--- components/division-select.tsx
+++ components/division-select.tsx
@@ -16,6 +16,48 @@ export type DivisionSelectProps = {
   isLoading: boolean
 };
 
+const conferenceNames: Record<PremierConferences, string> = {
+  [PremierConferences.EuCentralEast]: "Central & Eastern Europe",
+  [PremierConferences.EuWest]: "Western Europe",
+  [PremierConferences.EuMiddleEast]: "Middle East",
+  [PremierConferences.EuTurkey]: "Türkiye",
+  [PremierConferences.NaUsEast]: "US East",
+  [PremierConferences.NaUsWest]: "US West",
+  [PremierConferences.LatamNorth]: "Latin America North",
+  [PremierConferences.LatamSouth]: "Latin America South",
+  [PremierConferences.BrBrazil]: "Brazil",
+  [PremierConferences.KrKorea]: "Korea",
+  [PremierConferences.ApAsia]: "Asia",
+  [PremierConferences.ApJapan]: "Japan",
+  [PremierConferences.ApOceania]: "Oceania",
+  [PremierConferences.ApSouthAsia]: "South Asia"
+}
+
+const divisionNames = [
+  null,
+  null,
+  "Open 2",
+  "Open 3",
+  "Open 4",
+  "Open 5",
+  "Intermediate 1",
+  "Intermediate 2",
+  "Intermediate 3",
+  "Intermediate 4",
+  "Intermediate 5",
+  "Advanced 1",
+  "Advanced 2",
+  "Advanced 3",
+  "Advanced 4",
+  "Advanced 5",
+  "Elite 1",
+  "Elite 2",
+  "Elite 3",
+  "Elite 4",
+  "Elite 5",
+  "Contender"
+]
+
 export function DivisionSelect({ onSelect, isLoading }: DivisionSelectProps) {
   const {
     register,
@@ -26,13 +68,13 @@ export function DivisionSelect({ onSelect, isLoading }: DivisionSelectProps) {
   return (
     <form onSubmit={handleSubmit(onSelect)} className="flex w-full flex-wrap md:flex-nowrap gap-4">
       <Select
-        label="Conference"
+        label="Zone"
         {...register("conference", { required: true })}
       >
         {Object.values(PremierConferences)
           .map(conference => (
             <SelectItem key={conference} value={conference}>
-              {conference}
+              {conferenceNames[conference]}
             </SelectItem>
           ))}
       </Select>
@@ -40,11 +82,12 @@ export function DivisionSelect({ onSelect, isLoading }: DivisionSelectProps) {
         label="Division"
         {...register("division", { required: true })}
       >
-        {Array.from(Array(20).keys())
+        {Array.from(Array(21).keys())
           .map(i => i + 1)
+          .filter(i => i >= 2)
           .map(division => (
             <SelectItem key={division} value={division}>
-              {division.toString()}
+              {divisionNames[division]}
             </SelectItem>
           ))}
       </Select>
