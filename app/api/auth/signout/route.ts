18288d64d9bc14a130cfccec126d6f9fb1eb00ba
Eric Jiang <ejiang@amazon.com>
2023-11-17 20:45:28 -0700

    Fix build.

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,1 +1,9 @@
-export async function GET(request: Request) { }
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({
+    error: "Not yet implemented"
+  }, {
+    status: 501,
+  });
+}
