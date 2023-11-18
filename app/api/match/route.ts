de2a43bd5b84148dfb188e5262385d3977db3401
Eric Jiang <ejiang@amazon.com>
2023-11-17 19:53:04 -0700

    Handle parsing errors in match API.

--- app/api/match/route.ts
+++ app/api/match/route.ts
@@ -1,10 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ErrorResponseBody, Match } from "../types";
+import { z } from "zod";
 
 export const runtime = "edge";
 
 const endpoint = new URL("https://api.henrikdev.xyz");
 const matchUrl = new URL("valorant/v2/match/", endpoint);
+
+/**
+ * https://app.swaggerhub.com/apis-docs/Henrik-3/HenrikDev-API
+ */
+const henrikMatchResponseSchema = z.object({
+  data: z.object({
+    metadata: z.object({
+      map: z.string()
+    })
+  })
+})
 
 export async function GET(request: NextRequest): Promise<NextResponse<Match | ErrorResponseBody>> {
   const matchId = request.nextUrl.searchParams.get("matchId");
@@ -17,12 +29,33 @@ export async function GET(request: NextRequest): Promise<NextResponse<Match | Er
   }
 
   const response = await fetch(new URL(matchId, matchUrl));
-  const data = await response.json();
-  const match: Match = {
-    map: data.data.metadata.map
-  };
+
+  if (response.status !== 200) {
+    return NextResponse.json({
+      error: "unknown"
+    }, {
+      status: response.status,
+    });
+  }
+
+  const safeParseResult = henrikMatchResponseSchema.safeParse(await response.json());
+  if (!safeParseResult.success) {
+    return NextResponse.json({
+      error: "Parsing error"
+    }, {
+      status: 500,
+    });
+  }
+
+  const match = convertMatch(safeParseResult.data)
 
   return NextResponse.json(match, {
     status: response.status,
   });
+}
+
+function convertMatch(henrikMatch: z.infer<typeof henrikMatchResponseSchema>): Match {
+  return {
+    map: henrikMatch.data.metadata.map
+  };
 }
