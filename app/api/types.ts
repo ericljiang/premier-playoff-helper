8a463ebd4f7d29469368d8f69a7c83082952c8fa
Eric Jiang <eric.jiang2@gmail.com>
2023-12-01 13:12:53 -0700

    Convert matches from Henrik API.

--- app/api/types.ts
+++ app/api/types.ts
@@ -2,6 +2,52 @@ export type ErrorResponseBody = {
   error: string;
 };
 
+type Player = {
+  character: string;
+};
+
+type Team = "red" | "blue";
+
+type Location = {
+  x: number;
+  y: number;
+};
+
 export type Match = {
   map: string;
+  teams: {
+    red: {
+      won: boolean;
+      roundsWon: number;
+      roundsLost: number;
+      premierTeamId?: string;
+    };
+    blue: {
+      won: boolean;
+      roundsWon: number;
+      roundsLost: number;
+      premierTeamId?: string;
+    };
+  };
+  rounds: Array<{
+    winningTeam: Team;
+    playerStats: Array<{
+      puuid: string;
+      kills: Array<{
+        timeSinceRoundStartMillis: number;
+        victimPuuid: string;
+        victimTeam: Team;
+        victimLocation: Location;
+        playerLocations: Array<{
+          puuid: string;
+          team: Team;
+          location: Location;
+        }>;
+      }>;
+    }>;
+  }>;
+  players: {
+    red: Player[];
+    blue: Player[];
+  };
 };
