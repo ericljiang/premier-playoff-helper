34fcbdb044499ab77c2f911d83074be33c15a3a6
Eric Jiang <eric.jiang2@gmail.com>
2024-06-12 09:42:34 -0700

    Use dropdown menu for sign out.

--- components/auth/profile-button.tsx
+++ components/auth/profile-button.tsx
@@ -1,15 +1,18 @@
 "use client";
-import { AvatarIcon, Button, User } from "@nextui-org/react";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
 
-export function ProfileButton(props: { gameName?: string, tagLine?: string; }) {
-  return <Button onClick={() => signOut()}>
-    <User
-      name={props.gameName ?? "???"}
-      description={`#${props.tagLine ?? "???"}`}
-      avatarProps={{
-        fallback: <AvatarIcon />
-      }}
-    />
-  </Button>;
-}
+export const ProfileButton = (props: { gameName?: string, tagLine?: string; }) =>
+  <Dropdown>
+    <DropdownTrigger>
+      <Button className="flex flex-col gap-0">
+        <p>{props.gameName ?? "???"}</p>
+        <p className="text-tiny text-foreground-600">{`#${props.tagLine ?? "???"}`}</p>
+      </Button>
+    </DropdownTrigger>
+    <DropdownMenu>
+      <DropdownItem onPress={() => signOut()}>
+        Sign Out
+      </DropdownItem>
+    </DropdownMenu>
+  </Dropdown>;
