cbd2b74d59593a2a5ca8b4294b4a11fdfd558ec9
Eric Jiang <ejiang@amazon.com>
2024-07-21 10:29:36 -0700

    Fix build.

--- app/api/match/henrik.ts
+++ app/api/match/henrik.ts
@@ -9,15 +9,18 @@ const henrikTeamSchema = z.object({
   rounds_won: z.number(),
   rounds_lost: z.number(),
   roster: z.object({
-    id: z.string()
-  })
+    id: z.string(),
+  }),
 });
 const henrikPlayerSchema = z.object({
-  character: z.string()
+  puuid: z.string(),
+  name: z.string(),
+  tag: z.string(),
+  character: z.string(),
 });
 const locationSchema = z.object({
   x: z.number(),
-  y: z.number()
+  y: z.number(),
 });
 
 /**
@@ -26,40 +29,50 @@ const locationSchema = z.object({
 const henrikMatchResponseSchema = z.object({
   data: z.object({
     metadata: z.object({
-      map: z.string()
+      map: z.string(),
     }),
     teams: z.object({
       red: henrikTeamSchema,
-      blue: henrikTeamSchema
+      blue: henrikTeamSchema,
     }),
-    rounds: z.array(z.object({
-      winning_team: z.literal("Red").or(z.literal("Blue")),
-      player_stats: z.array(z.object({
-        player_puuid: z.string(),
-        kill_events: z.array(z.object({
-          kill_time_in_round: z.number(),
-          killer_puuid: z.string(),
-          victim_puuid: z.string(),
-          victim_display_name: z.string(),
-          victim_team: z.literal("Red").or(z.literal("Blue")),
-          victim_death_location: locationSchema,
-          player_locations_on_kill: z.array(z.object({
+    rounds: z.array(
+      z.object({
+        winning_team: z.literal("Red").or(z.literal("Blue")),
+        player_stats: z.array(
+          z.object({
             player_puuid: z.string(),
-            player_display_name: z.string(),
-            player_team: z.literal("Red").or(z.literal("Blue")),
-            location: locationSchema
-          }))
-        }))
-      }))
-    })),
+            kill_events: z.array(
+              z.object({
+                kill_time_in_round: z.number(),
+                killer_puuid: z.string(),
+                victim_puuid: z.string(),
+                victim_display_name: z.string(),
+                victim_team: z.literal("Red").or(z.literal("Blue")),
+                victim_death_location: locationSchema,
+                player_locations_on_kill: z.array(
+                  z.object({
+                    player_puuid: z.string(),
+                    player_display_name: z.string(),
+                    player_team: z.literal("Red").or(z.literal("Blue")),
+                    location: locationSchema,
+                  })
+                ),
+              })
+            ),
+          })
+        ),
+      })
+    ),
     players: z.object({
       red: z.array(henrikPlayerSchema),
-      blue: z.array(henrikPlayerSchema)
-    })
-  })
+      blue: z.array(henrikPlayerSchema),
+    }),
+  }),
 });
 
-function convertMatch(henrikMatch: z.infer<typeof henrikMatchResponseSchema>): Match {
+function convertMatch(
+  henrikMatch: z.infer<typeof henrikMatchResponseSchema>
+): Match {
   return {
     map: henrikMatch.data.metadata.map,
     teams: {
@@ -67,20 +80,20 @@ function convertMatch(henrikMatch: z.infer<typeof henrikMatchResponseSchema>): M
         won: henrikMatch.data.teams.red.has_won,
         roundsWon: henrikMatch.data.teams.red.rounds_won,
         roundsLost: henrikMatch.data.teams.red.rounds_lost,
-        premierTeamId: henrikMatch.data.teams.red.roster.id
+        premierTeamId: henrikMatch.data.teams.red.roster.id,
       },
       blue: {
         won: henrikMatch.data.teams.blue.has_won,
         roundsWon: henrikMatch.data.teams.blue.rounds_won,
         roundsLost: henrikMatch.data.teams.blue.rounds_lost,
-        premierTeamId: henrikMatch.data.teams.blue.roster.id
-      }
+        premierTeamId: henrikMatch.data.teams.blue.roster.id,
+      },
     },
-    rounds: henrikMatch.data.rounds.map(round => ({
+    rounds: henrikMatch.data.rounds.map((round) => ({
       winningTeam: round.winning_team === "Red" ? "red" : "blue",
-      playerStats: round.player_stats.map(playerStatsItem => ({
+      playerStats: round.player_stats.map((playerStatsItem) => ({
         puuid: playerStatsItem.player_puuid,
-        kills: playerStatsItem.kill_events.map(killEvent => ({
+        kills: playerStatsItem.kill_events.map((killEvent) => ({
           timeSinceRoundStartMillis: killEvent.kill_time_in_round,
           victimPuuid: killEvent.victim_puuid,
           victimTeam: killEvent.victim_team === "Red" ? "red" : "blue",
@@ -88,31 +101,44 @@ function convertMatch(henrikMatch: z.infer<typeof henrikMatchResponseSchema>): M
             x: killEvent.victim_death_location.x,
             y: killEvent.victim_death_location.y,
           },
-          playerLocations: killEvent.player_locations_on_kill.map(location => ({
-            puuid: location.player_puuid,
-            team: location.player_team === "Red" ? "red" : "blue",
-            location: location.location
-          }))
-        }))
-      }))
+          playerLocations: killEvent.player_locations_on_kill.map(
+            (location) => ({
+              puuid: location.player_puuid,
+              team: location.player_team === "Red" ? "red" : "blue",
+              location: location.location,
+            })
+          ),
+        })),
+      })),
     })),
     players: {
-      red: henrikMatch.data.players.red.map(player => ({
-        character: player.character
+      red: henrikMatch.data.players.red.map((player) => ({
+        puuid: player.puuid,
+        gameName: player.name,
+        tagLine: player.tag,
+        character: player.character,
       })),
-      blue: henrikMatch.data.players.blue.map(player => ({
-        character: player.character
-      }))
-    }
+      blue: henrikMatch.data.players.blue.map((player) => ({
+        puuid: player.puuid,
+        gameName: player.name,
+        tagLine: player.tag,
+        character: player.character,
+      })),
+    },
   };
 }
 
-export async function retrieveMatchFromHenrik(matchId: string, apiKey: string | undefined): Promise<Match> {
+export async function retrieveMatchFromHenrik(
+  matchId: string,
+  apiKey: string | undefined
+): Promise<Match> {
   const requestUrl = new URL(matchId, matchUrl);
   const response = await fetch(requestUrl, {
-    headers: apiKey ? {
-      Authorization: apiKey
-    } : undefined
+    headers: apiKey
+      ? {
+          Authorization: apiKey,
+        }
+      : undefined,
   });
   if (response.status !== 200) {
     throw Error(`Request to ${requestUrl} failed: ${await response.text()}`);
